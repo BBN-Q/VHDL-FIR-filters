2e44666922d9522093f672184da0d4910cb9d3bd
Colm Ryan <cryan@bbn.com>
2015-05-20 21:06:01 -0400

    :white_check_mark: add output data checking to testbench
    Already helped catch the bug in d603aaf30b74465240b00af1646e724f8b95552b

--- test/FIR_tb.vhd
+++ test/FIR_tb.vhd
@@ -13,6 +13,7 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
+use ieee.math_real.all;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
@@ -36,15 +37,20 @@ signal clk : std_logic := '0';
 signal finished : boolean := false;
 
 signal data_in : std_logic_vector(15 downto 0) := (others => '0');
-signal data_out : std_logic_vector(15 downto 0);
+signal data_out, data_check : std_logic_vector(15 downto 0) := (others => '0');
 
-constant DATA_WIDTH_IN := 16;
-constant DATA_IN_SCALE : real := real(2 ** (DATA_WIDTH_IN-1)) - 1.0;
+constant DATA_IN_WIDTH : natural := 16;
+constant DATA_IN_SCALE : real := real(2 ** (DATA_IN_WIDTH-1)) - 1.0;
+
+constant DATA_OUT_WIDTH : natural := 16;
+constant DATA_OUT_SCALE : real := real(2 ** (DATA_OUT_WIDTH-1)) - 1.0;
+
+constant FILTER_DELAY : natural := 2;
 
 begin
 
   dut : entity work.FIR_DirectTranspose
-    generic map(coeffs => coeffs, data_in_width=>DATA_WIDTH_IN, data_out_width=>16)
+    generic map(coeffs => coeffs, data_in_width=>DATA_IN_WIDTH, data_out_width=>DATA_OUT_WIDTH)
     port map (
       rst => rst,
       clk => clk,
@@ -57,11 +63,11 @@ begin
   begin
     rst <= '1';
     wait for 100ns;
-    rst <= '0';
-
     wait until rising_edge(clk);
-    sampleDriver : for ct in 0 to chirp'high loop
-      data_in <= std_logic_vector(to_signed(integer(SCALE_TO_INT*chirp(ct)), 16));
+    rst <= '0';
+    wait until rising_edge(clk);
+    sampleDriver : for ct in chirp'range loop
+      data_in <= std_logic_vector(to_signed(integer(DATA_IN_SCALE*chirp(ct)), 16));
       wait until rising_edge(clk);
     end loop;
     data_in <= (others => '0');
@@ -70,6 +76,33 @@ begin
     finished <= true;
   end process;
 
+  check : process
+  variable curOutput : real;
+  begin
+    wait for 100ns;
+    wait until rising_edge(clk);
+    wait until rising_edge(clk);
+
+    for ct in 0 to FILTER_DELAY loop
+      wait until rising_edge(clk);
+    end loop;
+    for ct in 0 to chirp'high + coeffs'high loop
+      curOutput := 0.0;
+      for tap in coeffs'range loop
+        if (ct-tap >= 0) and (ct-tap <= chirp'high) then
+          curOutput := curOutput + chirp(ct-tap)*coeffs(tap);
+        end if;
+      end loop;
+      data_check <= std_logic_vector(to_signed(integer(trunc(DATA_OUT_SCALE * curOutput)), 16));
+      --Arbitrarly allow 2 differences due to fixed point errors
+      assert abs(signed(data_check) - signed(data_out)) <= 2 report "FIR filter output incorrect!";
+      wait until rising_edge(clk);
+    end loop;
+    data_check <= (others => '0');
+
+    wait for 1us;
+
+  end process;
 
 
   --clock generation
